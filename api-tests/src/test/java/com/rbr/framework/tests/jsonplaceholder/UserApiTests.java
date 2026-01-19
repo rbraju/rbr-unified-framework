6d45ac345b49e5105eccc78409663e980e2f6ca7
Rajesh <rajesh.spk@gmail.com>
2026-01-19 14:56:07 -0800

    Use base url from properties

--- api-tests/src/test/java/com/rbr/framework/tests/jsonplaceholder/UserApiTests.java
+++ api-tests/src/test/java/com/rbr/framework/tests/jsonplaceholder/UserApiTests.java
@@ -9,17 +9,15 @@ import static com.rbr.framework.core.TestContext.api;
 
 public class UserApiTests extends BaseTest {
 
-    private static final String BASE_URL = "https://jsonplaceholder.typicode.com";
-
     @Test
     public void testGetUserById() {
-        var response = api().get(BASE_URL + "/users/1");
+        var response = api().get("/users/1");
         Assert.assertEquals(response.getStatusCode(), 200);
     }
 
     @Test
     public void createUser() {
-        var response = api().post(BASE_URL + "/users", UserUtil.getDefaultUser());
+        var response = api().post("/users", UserUtil.getDefaultUser());
         Assert.assertEquals(response.getStatusCode(), 201);
     }
 }
